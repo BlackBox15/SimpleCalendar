f7e46037802e0b89026d59c7503de49ecd8e7987
mk1221@yandex.ru <>
2022-05-25 11:56:37 +0300

    Initial commit.

---
+++ src/myprod/Calendar.java
@@ -0,0 +1,13 @@
+package myprod;
+
+public class Calendar {
+
+	public static void main(String[] args) {
+		
+		CallCalendar.getCalendar(Integer.parseInt(args[0]));
+//		CalendarFromSunday.getCalendar(Integer.parseInt(args[0]));
+
+	}
+	
+}
+	
